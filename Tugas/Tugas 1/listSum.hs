77b54ef41812376690693fdd576e765a364e8503
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-09-15 10:12:04 +0700

    tugas1: add listSum program

--- Tugas/Tugas 1/listSum.hs
+++ Tugas/Tugas 1/listSum.hs
@@ -1,1 +1,19 @@
--- ListSum
+{-
+Reference:
+    > https://stackoverflow.com/questions/34817219/haskell-recursive-list-summation
+    > https://stackoverflow.com/questions/43718678/read-strings-from-keyboard-in-haskell
+    > https://stackoverflow.com/questions/8879391/how-do-i-convert-string-into-list-of-integers-in-haskell
+-}
+
+listSum :: [Int] -> Int
+listSum [] = 0
+listSum (x:t) = x + listSum t
+
+seqOfStringToInt :: String -> [Int]
+seqOfStringToInt seq = map read $ words seq
+
+main:: IO()
+main = do
+    seq <- getLine
+    let listOfInt = seqOfStringToInt seq
+    print $ listSum listOfInt
