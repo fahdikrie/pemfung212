d1f1fce4032ded39a30e1674af5f4f42d48f3814
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-09-22 02:12:05 +0700

    tugas3: add answer to currys, fib, power, and sumeven

---
+++ Tugas/Tugas 3/Tugas3-1906398370-FahdiiAjmalalFikrie.hs
@@ -0,0 +1,42 @@
+{-
+    References:
+        1. https://stackoverflow.com/questions/27554272/how-do-you-define-and-use-curry-and-uncurry-prelude-functions-in-haskell
+        2. https://stackoverflow.com/questions/5863370/exponential-function-with-addition
+-}
+
+-- No. 1 — myCurry & myUncurry
+myCurry :: ((a, b) -> c) -> a -> b -> c
+myCurry f = \x y -> f (x, y)
+
+myUncurry :: (a -> b -> c) -> ((a, b) -> c)
+myUncurry f = \(x,y) -> f x y
+
+-- No. 2 — Fibonacci
+fibonacci :: Integer -> Integer
+fibonacci 0 = 1
+fibonacci 1 = 0
+fibonacci n = fibonacci(n-1) + fibonacci(n-2)
+
+fibonaccis :: Integer -> [Integer]
+fibonaccis n = map fibonacci [1..n]
+
+-- No. 3 — Power function w/ only addition
+multiply :: Integer -> Integer -> Integer
+multiply a 0 = 0
+multiply a 1 = a
+multiply a b = a + multiply a (b-1)
+
+power :: Integer -> Integer -> Integer
+power a 0 = 1
+power a 1 = a
+power a b = multiply a (power a (b-1))
+
+-- No. 4 — sumEven
+-- Kalo tipenya Real malah compile error :()
+sumEven :: [Integer] -> Integer
+sumEven [] = 0
+sumEven (x:xs)
+    | (x `mod` 2 == 0) = x + sumEven xs
+    | otherwise        = 0 + sumEven xs
+
+-- No. 5 — Kalkulator Investasi
